c7332f37f1193c0929b17c1118453502dd5f122c
Brij Shah <brijshah0@gmail.com>
2019-06-28 11:23:51 -0700

    updates to boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TF_VERSION=0.12.1
+TF_ZIP=terraform_"$TF_VERSION"_darwin_amd64.zip
+
 # Check for xcode-command-line tools, otherwise exit
 if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
    test -d "${xpath}" && test -x "${xpath}" ; then
@@ -9,20 +12,36 @@ else
    exit 1
 fi
 
-# Check for Homebrew, install if we don't have it
+# Check for Homebrew, install if it is not present
 if test ! $(which brew); then
     printf "Installing homebrew...\n"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 fi
 
 brew install antibody
-brew install zsh
-brew install terminal-notifier
-brew install python3
-brew install wget
 brew install git
+brew install python3
+brew install terminal-notifier
+brew install tree
+brew install wget
+brew install zsh
 
 grep -q -f $(which zsh) /etc/shells || echo $(which zsh) | sudo tee -a /etc/shells
 chsh -s $(which zsh)
 
+# For Macbooks with Touchbar
 wget -O "/Users/$USER/Downloads/pock.zip" "https://pock.dev/download.php?file=pock_0_5_1_lack.zip"
+
+# awscli
+pip3 install --user awscli
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# terraform
+curl -LO https://releases.hashicorp.com/terraform/"$TF_VERSION"/"$TF_ZIP"
+unzip $TF_ZIP
+mv terraform /usr/local/bin/
+rm $TF_ZIP
