4854c1d3ce4ee7e105e29505428b26c0f410af0f
Brij Shah <brijshah@Brijs-MacBook-Air.local>
2026-05-19 20:28:08 -0700

    update

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,20 @@
+SPACESHIP_PROMPT_ORDER=(
+    user
+    host
+    dir
+    venv
+    python
+    git
+    exec_time
+    jobs
+    exit_code
+    line_sep
+    char
+    terraform
+    node
+)
+
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+SPACESHIP_VENV_SHOW=true
